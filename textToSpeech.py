8fbae5d7550e88782bcfb6c98980f60a9b97190e
Pavel Lunev <pavellunev99@gmail.com>
2021-06-12 15:58:44 +0500

    some fixes

--- textToSpeech.py
+++ textToSpeech.py
@@ -1,32 +1,30 @@
 import requests, json, os, threading, pyttsx3, time
 
 class TelegramBot:
+
+    engine = pyttsx3.init()
+
     def __init__(self):
         self.__URL = 'https://api.telegram.org/bot1845322686:AAFytbyICZH76cz2SRYQ6l94XTvA8kZQA7M/'
         self.__last_update_id = 0
         self.__setLastUpdate()
+
+        voices = self.engine.getProperty('voices')
+        self.engine.setProperty('voice', 'ru')
+
+        for voice in voices:
+            if voice.name == 'Aleksandr':
+                self.engine.setProperty('voice', voice.id)
 
     def runUpdate(self):
         threading.Timer(3.0, self.runUpdate).start()
         self.index()
 
     def generateVoice(self, text):
-        engine = pyttsx3.init()
-
-        voices = engine.getProperty('voices')
-        engine.setProperty('voice', 'ru')
-
-        for voice in voices:
-            if voice.name == 'Aleksandr':
-                engine.setProperty('voice', voice.id)
-
-        # On linux make sure that 'espeak' and 'ffmpeg' are installed
-        engine.save_to_file(text, 'voice.mp3')
-        engine.runAndWait()
-        time.sleep(5)
+        self.engine.save_to_file(text, 'voice.mp3')
+        self.engine.runAndWait()
 
     def __setLastUpdate(self):
-        #not to send two responses
         response = requests.get(self.__URL + "getUpdates").json()
         result = response['result']
         if len(result) > 0:
@@ -39,12 +37,9 @@ class TelegramBot:
         result = response['result']
 
         if len(result) > 0:
-            #respond to all new messages, and save the last update_id to the session
             for update in result:
                 update_id = update['update_id']
                 if(update_id > self.__last_update_id):
-                    #line below should be here, otherwise bot sends us more than one replies,
-                    # because code doesn't catch up with the logic sometimes
                     self.__last_update_id = update_id
 
                     message = update['message']
@@ -60,7 +55,6 @@ class TelegramBot:
                         else:
                             self.generateVoice(text)
                             files = {'voice': open('voice.mp3','rb')}
-                            #send a voice message
                             print(requests.post('{}sendVoice?chat_id={}'.format(self.__URL, chat_id), files=files).json())
         return 0
 
