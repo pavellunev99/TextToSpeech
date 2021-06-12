41c0d8f7cd4d1ca44bca980ac1890a554be899b1
Pavel Lunev <pavellunev99@gmail.com>
2021-06-12 17:43:31 +0500

    this shit work

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from pydub import AudioSegment
 
 bot = telebot.TeleBot('1845322686:AAFytbyICZH76cz2SRYQ6l94XTvA8kZQA7M')
 fileName = "voice.ogg"
-fullPath = os.path.join(os.getcwd(), fileName)
+engine = pyttsx3.init()
 
 def change_voice(engine, language, gender= 'VoiceGenderFemale'):
     for voice in engine.getProperty('voices'):
@@ -20,17 +20,10 @@ def engine_settings(engine):
     engine.setProperty('rate', 185)  # Выставляем скорость чтения голоса
     change_voice(engine, "ru_RU", "VoiceGenderFemale")
 
-def get_mp3_file(text):
-    engine = pyttsx3.init()
-    #engine_settings(engine)  # Применение настроек голоса
-    engine.save_to_file(text, fullPath)  # Сохранение сообщения в аудиофайл
-    engine.runAndWait()
-    #convert_file_to_ogg('voice.ogg')  # Конвертация
-
 def convert_file_to_ogg():
     converter = AudioSegment
-    converter_file = converter.from_file(fullPath)
-    converter_file.export(fullPath, format="ogg")
+    converter_file = converter.from_file(fileName)
+    converter_file.export(fileName, format="ogg")
 
 @bot.message_handler(commands=['start'])
 def forward_message(message):
@@ -38,9 +31,10 @@ def forward_message(message):
 
 @bot.message_handler(content_types=['text'])
 def forward_message(message):
-    f = open(fullPath, 'rb')
-    get_mp3_file(message)
-    bot.send_audio(message.from_user.id, audio=f)
-    f.close()
+    engine_settings(engine)  # Применение настроек голоса
+    engine.save_to_file(message.text, fileName)  # Сохранение сообщения в аудиофайл
+    engine.runAndWait()
+    convert_file_to_ogg()  # Конвертация
+    bot.send_audio(message.from_user.id, audio=open(fileName, 'rb'))
 
 bot.polling()
