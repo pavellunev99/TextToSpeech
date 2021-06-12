48e95676f2305a11a76b05c5cc85b767722de06c
Pavel Lunev <pavellunev99@gmail.com>
2021-06-12 17:13:58 +0500

    fix

--- bot.py
+++ bot.py
@@ -1,8 +1,33 @@
 import telebot
-from voice import get_mp3_file
-
+import pyttsx3
+from pydub import AudioSegment
 
 bot = telebot.TeleBot('1845322686:AAFytbyICZH76cz2SRYQ6l94XTvA8kZQA7M')
+
+def change_voice(engine, language, gender='VoiceGenderFemale'):
+    for voice in engine.getProperty('voices'):
+        if language in voice.languages and gender == voice.gender:
+            engine.setProperty('voice', voice.id)
+            return True
+
+    raise RuntimeError("Language '{}' for gender '{}' not found".format(language, gender))
+
+def engine_settings(engine):
+    voices = engine.getProperty('voices')
+    engine.setProperty('rate', 185)  # Выставляем скорость чтения голоса
+    change_voice(engine, "ru_RU", "VoiceGenderFemale")
+
+def get_mp3_file(text):
+    engine = pyttsx3.init()
+    engine_settings(engine)  # Применение настроек голоса
+    engine.save_to_file(text, 'voice.ogg')  # Сохранение сообщения в аудиофайл
+    engine.runAndWait()
+    convert_file_to_ogg('voice.ogg')  # Конвертация
+
+def convert_file_to_ogg(file_name):
+    converter = AudioSegment
+    converter_file = converter.from_file(file_name)
+    converter_file.export(file_name, format="ogg")
 
 @bot.message_handler(commands=['start'])
 def forward_message(message):
@@ -10,8 +35,7 @@ def forward_message(message):
 
 @bot.message_handler(content_types=['text'])
 def forward_message(message):
-    file_name = 'voice.mp3'
-    get_mp3_file(file_name, message, 'ru_RU')
-    bot.send_audio(message.from_user.id, audio=open(file_name, 'rb'))
+    get_mp3_file(message)
+    bot.send_audio(message.from_user.id, audio=open('voice.ogg', 'rb'))
 
 bot.polling()
